661eb263c212c6508747fbdfef8c32b1bb60985d
nushakrishnan <anushakrishnan3005@gmail.com>
2024-11-07 02:14:27 +0100

    upd paths for mono too

--- Examples/Monocular-Inertial/mono_inertial_euroc.cc
+++ Examples/Monocular-Inertial/mono_inertial_euroc.cc
@@ -235,8 +235,8 @@ int main(int argc, char *argv[])
     // Save camera trajectory
     if (bFileName)
     {
-        const string kf_file = "/home/baselines/algorithms/orb/kf_" + string(argv[argc - 1]) + ".txt";
-        const string f_file = "/home/baselines/algorithms/orb/f_" + string(argv[argc - 1]) + ".txt";
+        const string kf_file = "/home/baselines/algorithms/orb/" + string(argv[argc - 1]) + ".txt";
+        const string f_file = "/home/baselines/algorithms/orb/" + string(argv[argc - 1]) + ".txt";
         SLAM.SaveTrajectoryEuRoC(f_file);
         SLAM.SaveKeyFrameTrajectoryEuRoC(kf_file);
     }
