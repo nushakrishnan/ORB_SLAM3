4b1b7ee80d285bb0918d34413c8420289afb780d
Anusha Krishnan <anushakrishnan3005@gmail.com>
2025-02-07 17:25:27 +0100

    mono euroc

--- Examples/Monocular/mono_euroc.cc
+++ Examples/Monocular/mono_euroc.cc
@@ -92,11 +92,12 @@ int main(int argc, char **argv)
         // Main loop
         cv::Mat im;
         int proccIm = 0;
+        cv::Ptr<cv::CLAHE> clahe = cv::createCLAHE(3.0, cv::Size(8, 8));
         for(int ni=0; ni<nImages[seq]; ni++, proccIm++)
         {
 
             // Read image from file
-            im = cv::imread(vstrImageFilenames[seq][ni],cv::IMREAD_UNCHANGED); //,CV_LOAD_IMAGE_UNCHANGED);
+            im = cv::imread(vstrImageFilenames[seq][ni],cv::IMREAD_GRAYSCALE); //,CV_LOAD_IMAGE_UNCHANGED);
             double tframe = vTimestampsCam[seq][ni];
 
             if(im.empty())
@@ -105,6 +106,9 @@ int main(int argc, char **argv)
                      <<  vstrImageFilenames[seq][ni] << endl;
                 return 1;
             }
+
+            // clahe
+            clahe->apply(im,im);
 
             if(imageScale != 1.f)
             {
