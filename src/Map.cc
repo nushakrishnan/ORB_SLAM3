6d3bfe2ca781e702faa8fa7a9e4e245fae18e67b
nushakrishnan <anushakrishnan3005@gmail.com>
2026-03-11 11:06:30 +0100

    segfault fix

--- src/Map.cc
+++ src/Map.cc
@@ -27,7 +27,8 @@ namespace ORB_SLAM3
 long unsigned int Map::nNextId=0;
 
 Map::Map():mnMaxKFid(0),mnBigChangeIdx(0), mbImuInitialized(false), mnMapChange(0), mpFirstRegionKF(static_cast<KeyFrame*>(NULL)),
-mbFail(false), mIsInUse(false), mHasTumbnail(false), mbBad(false), mnMapChangeNotified(0), mbIsInertial(false), mbIMU_BA1(false), mbIMU_BA2(false)
+mbFail(false), mIsInUse(false), mHasTumbnail(false), mbBad(false), mnMapChangeNotified(0), mbIsInertial(false), mbIMU_BA1(false), mbIMU_BA2(false),
+mpKFinitial(static_cast<KeyFrame*>(NULL)), mpKFlowerID(static_cast<KeyFrame*>(NULL))
 {
     mnId=nNextId++;
     mThumbnail = static_cast<GLubyte*>(NULL);
@@ -35,7 +36,8 @@ mbFail(false), mIsInUse(false), mHasTumbnail(false), mbBad(false), mnMapChangeNo
 
 Map::Map(int initKFid):mnInitKFid(initKFid), mnMaxKFid(initKFid),/*mnLastLoopKFid(initKFid),*/ mnBigChangeIdx(0), mIsInUse(false),
                        mHasTumbnail(false), mbBad(false), mbImuInitialized(false), mpFirstRegionKF(static_cast<KeyFrame*>(NULL)),
-                       mnMapChange(0), mbFail(false), mnMapChangeNotified(0), mbIsInertial(false), mbIMU_BA1(false), mbIMU_BA2(false)
+                       mnMapChange(0), mbFail(false), mnMapChangeNotified(0), mbIsInertial(false), mbIMU_BA1(false), mbIMU_BA2(false),
+                       mpKFinitial(static_cast<KeyFrame*>(NULL)), mpKFlowerID(static_cast<KeyFrame*>(NULL))
 {
     mnId=nNextId++;
     mThumbnail = static_cast<GLubyte*>(NULL);
